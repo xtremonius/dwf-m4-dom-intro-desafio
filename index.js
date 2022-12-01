ffc0fca692562726dccb203f8dfa0ae8d201579e
xtremonius <alejandro.aquino1988@gmail.com>
2022-12-01 17:21:37 -0300

    Desafio-capitulo-03

--- index.js
+++ index.js
@@ -25,6 +25,31 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const itemsEls = document.querySelectorAll(".lista li");
+  const listaEl = document.querySelector(".lista");
+
+  /**
+   * usando forEach como arrow fuction
+   
+  itemsEls.forEach((el) => {
+    console.log(el);
+  });
+
+   * usando for of
+   */
+  for (let el of itemsEls) {
+    //console.log(el);
+    el.remove();
+  }
+
+  for (let item of cosasQueAprendimos) {
+    const newLiEl = document.createElement("li");
+    newLiEl.textContent = item.tema;
+    const laClaseQueLeVoyAAsignarAlLi = item.class || "item";
+    newLiEl.classList.add(laClaseQueLeVoyAAsignarAlLi);
+    listaEl.appendChild(newLiEl);
+  }
+}
 
 main();
